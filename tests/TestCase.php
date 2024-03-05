327cf78a157a0845456fce725f7d77009a62f7b8
Aymane D. Hlamnach <dara-@outlook.jp>
2024-03-05 11:57:39 +0100

    foggen: feature callback generation

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use Illuminate\Contracts\Config\Repository;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 use YouCanShop\Foggle\Foggle;
@@ -14,5 +15,15 @@ abstract class TestCase extends OrchestraTestCase
     {
         return new Foggle($this->app);
     }
-    //
+
+    protected function defineEnvironment($app)
+    {
+        tap($app['config'], function (Repository $config) {
+            $config->set('features', [
+                'billing' => [
+                    'sellers' => splode('1,2,3'),
+                ],
+            ]);
+        });
+    }
 }
